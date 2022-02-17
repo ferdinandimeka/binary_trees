2295adb7f0b19f7b97ef382b53bc86c31683c1c2
Imekaferdy <Imekaferdy20@gmail.com>
2022-02-17 07:31:18 -0800

    update

--- binary_trees.h
+++ binary_trees.h
@@ -22,6 +22,15 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+/*Binary search tree*/
+typedef struct binary_tree_s bst_t;
+
+/* AVL tree*/
+typedef struct binary_tree_s avl_t;
+
+/*max binary heap*/
+typedef struct binary_tree_s heap_t;
+
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
